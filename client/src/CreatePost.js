d9e33cef7ea55af2c5881fdb388e5a88c4e2871c
logith vikram <logithvikram2982002@gmail.com>
2024-02-07 12:00:08 +0530

    second

---
+++ client/src/CreatePost.js
@@ -0,0 +1,7 @@
+export default function CreatePost(){
+    return (<form>
+        <input type = "title" placeholder={'Title'}></input>
+        <input type = "summary" placeholder={'Summary'}></input>
+        <input type = "file"/>
+       </form>)
+}
